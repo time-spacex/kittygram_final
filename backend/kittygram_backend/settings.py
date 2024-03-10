fb304012afab9d506f0080664473163eb6b540dc
Роман Клешнев <time-space@yandex.ru>
2024-03-10 15:45:06 +0300

    Fix settings.py, add variables in .env, fix docker-compose files, fix github actions

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,6 +1,8 @@
 # flake8: noqa
 import os
 from pathlib import Path
+
+from django.core.management.utils import get_random_secret_key
 from dotenv import load_dotenv
 
 
@@ -8,13 +10,11 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
+SECRET_KEY = os.getenv('SECRET_KEY', default=get_random_secret_key())
 
-SECRET_KEY = os.getenv('SECRET_KEY', default='default')
+DEBUG = os.getenv('DEBUG', default=False)
 
-DEBUG = True
-
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default='127.0.0.1').split(', ')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
